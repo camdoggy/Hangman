fb69f3e39c30f94f0ae353efaa5228c6be1fc2aa
SoulxDragon <SoulxDragon@gmail.com>
2014-11-25 10:29:57 -0700

    Hey look

--- hangman.py
+++ hangman.py
@@ -49,11 +49,12 @@ HANGMANPICS = ['''
  / \  |
       |
 =========''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
-def getRandomWord(wordList):
-    # This function returns a random string from the passed list of strings.
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return wordList[wordIndex]
+
+def getRandomWord():
+  text_file = open("dictionary.txt", "r")
+  word = text_file.readlines()
+  return random.choice(word).strip()
+
 def displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord):
     print(HANGMANPICS[len(missedLetters)])
     print()
